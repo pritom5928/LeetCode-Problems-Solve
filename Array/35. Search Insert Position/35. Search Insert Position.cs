dfa6282c498a81c40bdce5725d6e93474a28cfb8
pritom5928 <paul15-5928@diu.edu.bd>
2022-09-15 13:53:26 +0600

    35. Search Insert Position solved in quite optimal by loop

--- Array/35. Search Insert Position/35. Search Insert Position.cs
+++ Array/35. Search Insert Position/35. Search Insert Position.cs
@@ -59,3 +59,27 @@ public class Solution
         return GetIndexOfSearch(nums, target, start, end);
     }
 }
+
+
+Quite optimal solution faster than 13.23% C# submission:
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int start = 0, end = nums.Length - 1;
+        while (start <= end)
+        {
+            int mid = (start + end)  / 2;
+
+            if (nums[mid] < target)
+                start = mid + 1;
+            else if (nums[mid] > target)
+                end = mid - 1;
+            else
+                return mid;
+        }
+        return start;
+    }
+}
+
